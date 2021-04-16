edeba43ed000d1ded9a4c4fc1764ffb9ead4fcc3
Agustin Del Torto <aguss15dt@gmail.com>
2021-04-16 20:11:23 -0300

    intento 1

--- src/App.js
+++ src/App.js
@@ -1,36 +1,85 @@
 import "./App.css";
-import { useTimer } from "react-timer-hook";
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from "react";
 
-function Timer({ sessionTime, breakTime }) {
-  let timestamp = new Date();
-  timestamp.setSeconds(timestamp.getSeconds() + sessionTime);
-  useEffect(() =>
-    {timestamp = new Date();
-    timestamp.setSeconds(timestamp.getSeconds() + sessionTime);},[sessionTime, timestamp])
-
-  const {
-    seconds,
-    minutes,
-    hours,
-    days,
-    isRunning,
-    start,
-    pause,
-    resume,
-    restart,
-  } = useTimer({ timestamp, onExpire: () => console.warn("onExpire called") });
-
-  const startStop = () => {isRunning ? pause() : start();};
+function Timer({ sessionMins, breakMins }) {
+  const [timer, setTimer] = useState();
+  const [seconds, setSeconds] = useState(0);
+  const [minutes, setMinutes] = useState(sessionMins);
+  const [isRunning, setRunningState] = useState(false);
+  const [hasStarted, setStartedState] = useState(false);
+  const [breakStarted, setBreakStarted] = useState(false);
   
+  const tick = useCallback(()=>{
+    console.log("tick", seconds, minutes)
+    if (seconds > 0) {
+      console.log("tick s", seconds, timer)
+      setSeconds(seconds - 1);
+    } else if (minutes > 0) {
+      setMinutes(minutes - 1);
+      setSeconds(59);
+      console.log("tick m", minutes, seconds, timer)
+    } else if (!breakStarted) {
+      setBreakStarted(true);
+      setMinutes(breakMins);
+    } else {
+      clearInterval(timer);
+    }
+  }, [breakMins, breakStarted, minutes, seconds])
+
+
+  
+  function restart() {
+    pause();
+    setSeconds(0);
+    setMinutes(sessionMins);
+    setStartedState(false);
+  }
+
+  function start() {
+    setTimer(setInterval(tick, 1000))
+    setRunningState(true);
+    setStartedState(true);
+  }
+
+  function resume() {
+    if (!hasStarted) return;
+    setRunningState(true);
+    setTimer(setInterval(tick, 1000));
+  }
+  function pause() {
+    if (!hasStarted) return;
+    setRunningState(false);
+    clearInterval(timer);
+  }
+
+  const startStop = () => {
+    if (isRunning) {
+      pause();
+      console.log("pauso");
+    } else if (hasStarted) {
+      resume();
+      console.log("resume");
+    } else {
+      start();
+      console.log("start");
+    }
+  };
 
   return (
     <>
-      <div id="timer-label">Session {isRunning}</div>
-      <div id="time-left">{minutes}:{seconds}</div>
-      <button id="start_stop" onClick={startStop}>START/STOP</button>
-      <button id="reset" onClick={() => {const time = new Date();
-  time.setSeconds(time.getSeconds() + sessionTime); restart(time)}}>RESET</button>
+      <div id="timer-label">Session {String(isRunning)}</div>
+      <div id="time-left">
+        {minutes}:{seconds === 0 ? "00" : seconds}
+      </div>
+      <button id="start_stop" onClick={startStop}>
+        START/STOP
+      </button>
+      <button
+        id="reset"
+        onClick={restart}
+      >
+        RESET
+      </button>
     </>
   );
 }
@@ -41,32 +90,40 @@ function App() {
 
   const incrementBreak = () => {
     setBreakMins(breakMins + 1);
-  }
+  };
   const incrementSession = () => {
     setSessionMins(sessionMins + 1);
-  }
+  };
   const decrementBreak = () => {
     setBreakMins(breakMins - 1);
-  }
+  };
   const decrementSession = () => {
     setSessionMins(sessionMins - 1);
-  }
+  };
 
   return (
     <>
       <div id="break-label">
         Break Length
         <div id="break-length">{breakMins}</div>
-        <button id="break-increment" onClick={incrementBreak}>+</button>
-        <button id="break-decrement" onClick={decrementBreak}>-</button>
+        <button id="break-increment" onClick={incrementBreak}>
+          +
+        </button>
+        <button id="break-decrement" onClick={decrementBreak}>
+          -
+        </button>
       </div>
       <div id="session-label">
         Session Length
         <div id="session-length">{sessionMins}</div>
-        <button id="session-increment" onClick={incrementSession}>+</button>
-        <button id="session-decrement" onClick={decrementSession}>-</button>
+        <button id="session-increment" onClick={incrementSession}>
+          +
+        </button>
+        <button id="session-decrement" onClick={decrementSession}>
+          -
+        </button>
       </div>
-      <Timer sessionTime = {sessionMins * 60} breakTime = {breakMins * 60}/>
+      <Timer sessionMins={sessionMins} breakMins={breakMins} />
     </>
   );
 }
